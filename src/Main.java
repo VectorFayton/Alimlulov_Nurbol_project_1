62d78374f8db99cb3c24e9a11f0f99064f4e43bc
Alimkulov Nurbol <124434364+VectorFayton@users.noreply.github.com>
2023-02-23 21:36:04 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+class Employee
+{
+    private String Employee_Name;
+    private int Employee_Age;
+    private String Employee_Email;
+    private int Employee_Position;
+    private int Employee_ID;
+    private float Employee_Salary;
+    private long Employee_Contact;
+
+    public void getInfo(){
+        try {
+
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
