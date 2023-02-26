cdbf1e563895cb94e009c482c2589460bfe47964
Alimkulov Nurbol <124434364+VectorFayton@users.noreply.github.com>
2023-02-26 22:31:11 +0600

    Add some exception.

--- src/Employee_Update.java
+++ src/Employee_Update.java
@@ -7,7 +7,9 @@ public class Employee_Update {
 
     public static void update_File(int ID, String key_word, String changeable_element) {
         String[] data_file = Employee_Show.read_File(ID);
-        System.out.println(data_file);
+        String file_name = String.format("file%s.txt", ID);
+        System.out.println(file_name);
+        File file = new File(file_name);
         switch (key_word) {
             case "Name":
                 data_file[0] = changeable_element;
@@ -23,6 +25,10 @@ public class Employee_Update {
                 break;
             case "ID":
                 data_file[4] = changeable_element;
+                String changed_file_name = String.format("file%s.txt", data_file[4]);
+                boolean b = file.delete();
+                System.out.println(b);
+                file = new File(changed_file_name);
                 break;
             case "Salary":
                 data_file[5] = changeable_element;
@@ -31,14 +37,10 @@ public class Employee_Update {
                 data_file[6] = changeable_element;
                 break;
         }
-        System.out.println(data_file);
         try {
-            String file_name = String.format("file%s.txt", ID);
-            Employee_Remove.Delete_File(file_name);
-            String changed_file_name = String.format("file%s.txt", data_file[4]);
-            FileWriter file = new FileWriter(changed_file_name);
-            file.write(String.format("%s \n%s \n%s \n%s \n%s \n%s \n%s", data_file[0], data_file[1], data_file[2], data_file[3], data_file[4], data_file[5], data_file[6]));
-            file.close();
+            FileWriter writer = new FileWriter(file);
+            writer.write(String.format("%s \n%s \n%s \n%s \n%s \n%s \n%s", data_file[0], data_file[1], data_file[2], data_file[3], data_file[4], data_file[5], data_file[6]));
+            writer.close();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
